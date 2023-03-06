b3a6aa571fce0cb076904cc11d886048be94acdf
Joey Groves <joeygroves@joeys-mbp.home>
2023-03-06 16:24:09 +0000

    Added POST API for adding a new ToDo

--- api/server.js
+++ api/server.js
@@ -18,6 +18,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
     .then(() => console.log("Connected to DB"))
     .catch(console.error);
 
+// Uses ToDo.js file to initialise and get ready to use database
 const Todo = require('./models/Todo');
 
 // It will find our ToDos and pass them back to ToDo file
@@ -25,6 +26,18 @@ app.get('/todos', async (req, res) => {
     const todos = await Todo.find();
 
     res.json(todos);
+});
+
+// Creates a new ToDo task, we only pass text because it's the only required field
+app.post('/todo/new', (req, res) => {
+    const todo = new Todo({
+        text: req.body.text
+    });
+
+    // Saves the ToDo to the collection
+    todo.save();
+
+    res.json(todo);
 });
 
 app.listen(3001, () => console.log("Server started on port 3001"));
