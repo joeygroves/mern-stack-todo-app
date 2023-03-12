a3e3e120d3810fea6f83ed355d23dd6e86d833fd
Joey Groves <joeygroves@Joeys-MacBook-Pro.local>
2023-03-12 14:48:24 +0900

    Added example todos

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,26 @@
 function App() {
     return (
         <div className="App">
-            
+            <h1>Welcome, Joey</h1>
+            <h4>Your Tasks</h4>
+
+            <div className="todos">
+                <div className="todo">
+                    <div className="checkbox"></div>
+                    
+                    <div className="text">Get milk</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+
+                <div className="todo is-complete">
+                    <div className="checkbox"></div>
+
+                    <div className="text">Order hanko</div>
+
+                    <div className="delete-todo">x</div>
+                </div>
+            </div>
         </div>
     );
 }
